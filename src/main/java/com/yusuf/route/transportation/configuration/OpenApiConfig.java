e6ba96027b8906d22898e9aab137f7e966d2417a
yusuf <ysferdgnn@gmail.com>
2026-03-02 20:05:12 +0300

    remove comment

--- src/main/java/com/yusuf/route/transportation/configuration/OpenApiConfig.java
+++ src/main/java/com/yusuf/route/transportation/configuration/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
     info = @Info(title = "API", version = "v1"),
-    security = { @SecurityRequirement(name = "bearerAuth") } // tüm endpoint'lere uygular
+    security = { @SecurityRequirement(name = "bearerAuth") }
 )
 @SecurityScheme(
     name = "bearerAuth",
